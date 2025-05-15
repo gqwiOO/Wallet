ff771997d1ac2ad2a4046585b97cb16121c959f1
l2fx6 <tverdokhlibov.volodymyr@raccoons.games>
2025-05-16 01:19:36 +0300

    Base Settings, db added, test controller added

---
+++ WalletServicee/Database/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WalletServicee.Database;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+}
---
+++ WalletServicee/Database/User.cs
@@ -0,0 +1,7 @@
+﻿namespace WalletServicee.Database;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+}
